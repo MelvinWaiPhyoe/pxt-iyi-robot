c3b5136a1ae444f7ed8964dc58ba65226544344c
MelvinWaiPhyoe <38678940+MelvinWaiPhyoe@users.noreply.github.com>
2020-07-30 21:18:04 +0630

    Initial commit

---
+++ main.ts
@@ -0,0 +1,323 @@
+/**
+ *iyi robot အတွက် MakeCode Extension ဖြစ်ပါသည်။
+ */
+
+//% weight=10 color="#FF6600" icon="\u2606" block="iyi" advanced=false
+//% groups="['Motors', 'Acturators', 'Sensors', 'Displays']"
+namespace motor {
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const PRESCALE = 0xFE
+    const LED0_ON_L = 0x06
+    
+    /**
+     * Servo Motor Input Pin ရွေးချယ်ရန်ဖြစ်ပါသည်။
+     */
+    export enum Servos {
+        S1 = 0x08,
+        S2 = 0x07,
+        S3 = 0x06,
+        S4 = 0x05,
+        S5 = 0x04,
+        S6 = 0x03,
+        S7 = 0x02,
+        S8 = 0x01
+    }
+
+    /**
+     * DC Motor Pin ရွေးချယ်ရန်ဖြစ်ပါသည်။
+     */
+    export enum Motors {
+        M1 = 0x1,
+        M2 = 0x2,
+        M3 = 0x3,
+        M4 = 0x4
+    }
+
+    /**
+     * Motor Rotation Direction ရွေးချယ်ရန်ဖြစ်ပါသည်။
+     */
+    export enum Dir {
+        //% blockId="CW" block="ရှေ့သို့"
+        CW = 1,
+        //% blockId="CCW" block="နောက်သို့"
+        CCW = -1,
+    }
+
+    
+
+    let initialized = false
+    
+    function i2cWrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cCmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cRead(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cWrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval;//Math.floor(prescaleval + 0.5);
+        let oldmode = i2cRead(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cWrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cWrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cWrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cWrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    }
+
+
+    
+
+
+    /**
+	 * Servo Motorအတွက် Control Function ဖြစ်ပါသည်။
+	*/
+    //% blockId=motor_servo 
+    //% block="Servo Motor Pin ရွေးချယ်ရန်|%index|Servo Motor Degree ရွေးချယ်ရန်|%degree"
+    //% group="Acturators"
+    //% degree.min=0 degree.max=180
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=4
+    export function servo(index: Servos, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        // 50hz
+        let v_us = (degree * 1800 / 180 + 600) // 0.6ms ~ 2.4ms
+        let value = v_us * 4096 / 20000
+        setPwm(index + 7, 0, value)
+    }
+
+    /**
+	 * DC Motor အတွက် Control Function ဖြစ်ပါသည်။
+    */
+    //% group="Motors"
+    //% weight=90
+    //% blockId=motor_MotorRun 
+    //% block="Motor Pinရွေးချယ်ရန်|%index|directonရွေးချယ်ရန်|%Dir|အမြန်နှုန်းရွေးချယ်ရန်|%speed"
+    //% speed.min=0 speed.max=255
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
+    export function MotorRun(index: Motors, direction: Dir, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        speed = speed * 16 * direction; // map 255 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= -4096) {
+            speed = -4095
+        }
+        if (index > 4 || index <= 0)
+            return
+        let pn = (4 - index) * 2
+        let pp = (4 - index) * 2 + 1
+        if (speed >= 0) {
+            setPwm(pp, 0, speed)
+            setPwm(pn, 0, 0)
+        } else {
+            setPwm(pp, 0, 0)
+            setPwm(pn, 0, -speed)
+        }
+    }
+
+    
+
+    /**
+	 * မိမိနှစ်သက်ရာ Motorကိုရပ်စေရန်ဖြစ်ပါသည်။
+    */
+    //% weight=80
+    //% group="Motors"
+    //% blockId=motor_motorStop block="ရပ်လိုသည့် Motor Pinကိုရွေးချယ်ပါ|%index"
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2 
+    export function motorStop(index: Motors) {
+        setPwm((4 - index) * 2, 0, 0);
+        setPwm((4 - index) * 2 + 1, 0, 0);
+    }
+
+    /**
+	 * DC Motorအားလုံးရပ်ရန်ဖြစ်ပါသည်။
+    */
+    //% weight=70
+    //% group="Motors"
+    //% blockId=motor_motorStopAll block="DC Motor အားလုံးရပ်ရန်"
+    export function motorStopAll(): void {
+        for (let idx = 1; idx <= 4; idx++) {
+            motorStop(idx);
+        }
+    }
+
+    let _temperature: number = -999.0
+    let _humidity: number = -999.0
+    let _readSuccessful: boolean = false
+
+    /**
+     * DHT11/DHT22 sensorအားစတင်အသုံးပြုနိုင်ရန်ဖြစ်ပါသည်။
+     */
+    //% blockId=DHTSensorStart
+    //% block="DHTအမျိုးအစားရွေးချယ်ရန် $DHT|Data pin $dataPin|Pin pull up $pullUp|Serial output $serialOtput|Wait 2 sec after query $wait"
+    //% pullUp.defl=true
+    //% serialOtput.defl=false
+    //% wait.defl=true
+    //% blockExternalInputs=true
+    //% group="Sensors"
+    export function queryData(DHT: DHTtype, dataPin: DigitalPin, pullUp: boolean, serialOtput: boolean, wait: boolean) {
+
+        //initialize
+        let startTime: number = 0
+        let endTime: number = 0
+        let checksum: number = 0
+        let checksumTmp: number = 0
+        let dataArray: boolean[] = []
+        let resultArray: number[] = []
+        for (let index = 0; index < 40; index++) dataArray.push(false)
+        for (let index = 0; index < 5; index++) resultArray.push(0)
+        _humidity = -999.0
+        _temperature = -999.0
+        _readSuccessful = false
+
+        startTime = input.runningTimeMicros()
+
+        //request data
+        pins.digitalWritePin(dataPin, 0) //begin protocol
+        basic.pause(18)
+        if (pullUp) pins.setPull(dataPin, PinPullMode.PullUp) //pull up data pin if needed
+        pins.digitalReadPin(dataPin)
+        control.waitMicros(20)
+        while (pins.digitalReadPin(dataPin) == 1);
+        while (pins.digitalReadPin(dataPin) == 0); //sensor response
+        while (pins.digitalReadPin(dataPin) == 1); //sensor response
+
+        //read data (5 bytes)
+        for (let index = 0; index < 40; index++) {
+            while (pins.digitalReadPin(dataPin) == 1);
+            while (pins.digitalReadPin(dataPin) == 0);
+            control.waitMicros(28)
+            //if sensor pull up data pin for more than 28 us it means 1, otherwise 0
+            if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+        }
+
+        endTime = input.runningTimeMicros()
+
+        //convert byte number array to integer
+        for (let index = 0; index < 5; index++)
+            for (let index2 = 0; index2 < 8; index2++)
+                if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+
+        //verify checksum
+        checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+        checksum = resultArray[4]
+        if (checksumTmp >= 512) checksumTmp -= 512
+        if (checksumTmp >= 256) checksumTmp -= 256
+        if (checksum == checksumTmp) _readSuccessful = true
+
+        //read data if checksum ok
+        if (_readSuccessful) {
+            if (DHT == DHTtype.DHT11) {
+                //DHT11
+                _humidity = resultArray[0] + resultArray[1] / 100
+                _temperature = resultArray[2] + resultArray[3] / 100
+            } else {
+                //DHT22
+                let temp_sign: number = 1
+                if (resultArray[2] >= 128) {
+                    resultArray[2] -= 128
+                    temp_sign = -1
+                }
+                _humidity = (resultArray[0] * 256 + resultArray[1]) / 10
+                _temperature = (resultArray[2] * 256 + resultArray[3]) / 10 * temp_sign
+            }
+        }
+
+        //serial output
+        if (serialOtput) {
+            let DHTstr: string = ""
+            if (DHT == DHTtype.DHT11) DHTstr = "DHT11"
+            else DHTstr = "DHT22"
+            serial.writeLine(DHTstr + " query completed in " + (endTime - startTime) + " microseconds")
+            if (_readSuccessful) {
+                serial.writeLine("Checksum ok")
+                serial.writeLine("Humidity: " + _humidity + " %")
+                serial.writeLine("Temperature: " + _temperature + " *C")
+            } else {
+                serial.writeLine("Checksum error")
+            }
+            serial.writeLine("----------------------------------------")
+        }
+
+        //wait 2 sec after query if needed
+        if (wait) basic.pause(2000)
+
+    }
+
+    /**
+     * DHT11/22 sensorမှ data ရယူရန်အတွက်ဖြစ်ပါသည်။
+     */
+    //% blockId=DHTSensorRead
+    //% block="DHTမှdataဖတ်ရန် $data"
+    //% group="Sensors"
+    export function readData(data: dataType): number {
+        return data == dataType.humidity ? _humidity : _temperature
+    }
+
+    /**
+     * Tracker Sensor မှ data ရယူရန်အတွက်ဖြစ်ပါသည်။
+     */
+    //% blockId=TrackerSensorRead
+    //% block="tracker sensor |%pin|"
+    //% group="Sensors"
+    export function trackline(pin: DigitalPin): number {
+        return pins.digitalReadPin(pin);
+    }
+}
+
+enum DHTtype {
+    //% block="DHT11"
+    DHT11,
+    //% block="DHT22"
+    DHT22,
+}
+
+enum dataType {
+    //% block="စိုထိုင်းဆ"
+    humidity,
+    //% block="အပူချိန််"
+    temperature,
+}
